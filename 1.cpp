02efeb0e5306c2bf1d23a9a47c012afb1481c122
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-10-29 15:39:46 +0800

    updates

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
 #include <opencv2/opencv.hpp>
 
 cv::Mat GaussianNoise(char* image_path, double mean, double stddev);
 
 int main()
 {
-    cv::Mat result = GaussianNoise("C:\\Users\\Lenovo\\projects\\opencvtest\\lenna.png", 125, 10);
+    cv::Mat result = GaussianNoise("C:\\Users\\Lenovo\\projects\\opencvtest\\lenna.png", 0, 0.1);
+    cv::imshow("OUTPUT",result);
+    cv::waitKey(0);
+    cv::medianBlur(result,result,3);
+    // arithmetric mean filter
     cv::imshow("OUTPUT",result);
     cv::waitKey(0);
     return 0;
@@ -15,6 +18,7 @@ cv::Mat GaussianNoise(char* image_path, double mean, double stddev)
 {
     cv::Mat input = cv::imread(image_path);
     cv::Mat imGray;
+    cv::Mat result;
     cv::cvtColor(input,imGray,cv::COLOR_BGR2GRAY);
     // COLOR_BGR2GRAY : convert between RGB/BGR and grayscale
     // int cv::Mat::type() const
@@ -22,9 +26,18 @@ cv::Mat GaussianNoise(char* image_path, double mean, double stddev)
     // imGray.type() ： CV_8U
     // Each pixel in a CV_8U image is represented by a single byte
     // , which can store values from 0 to 255.
-    cv::Mat noise = cv::Mat(imGray.size(),imGray.type());
+    // CV_64F:
+    // CV_ - this is just a prefix
+    // 64 - number of bits per base matrix element
+    // F - type of the base element.
+    // F (float)
+    // S (signed)
+    // U (unsigned)
+    cv::Mat noise = cv::Mat(imGray.size(),CV_64F);
+    cv::normalize(imGray,result,0,1.0,cv::NORM_MINMAX,CV_64F);
     cv::randn(noise,mean,stddev);
-    cv::Mat result;
-    result = imGray + noise;
+    result = result + noise;
+    cv::normalize(result,result,0,1.0,cv::NORM_MINMAX,CV_64F);
+    result.convertTo(result, CV_8U, 255, 0);
     return result;
 }
