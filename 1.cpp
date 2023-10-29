98143be6c692a985e788f24f3f8f0c7cdb1dcb4a
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-10-29 11:59:53 +0800

    updates

--- 1.cpp
+++ 1.cpp
@@ -16,6 +16,12 @@ cv::Mat GaussianNoise(char* image_path, double mean, double stddev)
     cv::Mat input = cv::imread(image_path);
     cv::Mat imGray;
     cv::cvtColor(input,imGray,cv::COLOR_BGR2GRAY);
+    // COLOR_BGR2GRAY : convert between RGB/BGR and grayscale
+    // int cv::Mat::type() const
+    // Returns the type of a matrix element.
+    // imGray.type() ： CV_8U
+    // Each pixel in a CV_8U image is represented by a single byte
+    // , which can store values from 0 to 255.
     cv::Mat noise = cv::Mat(imGray.size(),imGray.type());
     cv::randn(noise,mean,stddev);
     cv::Mat result;
