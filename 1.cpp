e59b6b6e25687d57709bd790864075a7a9c722cb
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-11-07 15:43:56 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,15 +1,24 @@
 #include <opencv2/opencv.hpp>
 
 cv::Mat GaussianNoise(char* image_path, double mean, double stddev);
+cv::Mat SaltPepperNoise(char* image_path, double pa, double pb);
 
 int main()
 {
-    cv::Mat result = GaussianNoise("C:\\Users\\Lenovo\\projects\\opencvtest\\lenna.png", 0, 0.1);
+    cv::Mat result = SaltPepperNoise("C:\\Users\\Lenovo\\projects\\opencvtest\\lenna.png", 0.1, 0.1);
+    cv::Mat result1;
+    cv::Mat result2;
+    cv::Mat result3;
     cv::imshow("OUTPUT",result);
     cv::waitKey(0);
-    cv::medianBlur(result,result,3);
-    // arithmetric mean filter
-    cv::imshow("OUTPUT",result);
+    cv::medianBlur(result,result1,3);
+    cv::imshow("OUTPUT",result1);
+    cv::waitKey(0);
+    cv::GaussianBlur(result,result2,cv::Size(3,3),0,0);
+    cv::imshow("OUTPUT",result2);
+    cv::waitKey(0);
+    cv::bilateralFilter(result,result3,1,1,1);
+    cv::imshow("OUTPUT",result3);
     cv::waitKey(0);
     return 0;
 }
@@ -40,4 +49,24 @@ cv::Mat GaussianNoise(char* image_path, double mean, double stddev)
     cv::normalize(result,result,0,1.0,cv::NORM_MINMAX,CV_64F);
     result.convertTo(result, CV_8U, 255, 0);
     return result;
+}
+
+
+cv::Mat SaltPepperNoise(char* image_path, double pp, double ps)
+{
+    cv::RNG rng;
+    cv::Mat input = cv::imread(image_path);
+    cv::Mat imGray;
+    cv::cvtColor(input,imGray,cv::COLOR_BGR2GRAY);
+    int amount1=int(input.rows*input.cols*pp);
+    int amount2=int(input.rows*input.cols*ps);
+    for(int counter=0; counter<amount1; ++counter)
+    {
+        imGray.at<uchar>(rng.uniform( 0,imGray.rows), rng.uniform(0, imGray.cols)) =0;
+    }
+    for (int counter=0; counter<amount2; ++counter)
+    {
+        imGray.at<uchar>(rng.uniform( 0,imGray.rows), rng.uniform(0, imGray.cols)) = 255;
+    }
+    return imGray;
 }
