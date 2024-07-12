b66e27e5203bb27596e50880489dd740bf82bd56
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2024-07-12 22:15:26 +0800

    updates

---
+++ createBackgroundSubtractorMOG2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <sstream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+const char* params
+ = "{ help h | | Print usage }"
+ "{ input | vtest.avi | Path to a video or a sequence of image }"
+ "{ algo | MOG2 | Background subtraction method (KNN, MOG2) }";
+int main(int argc, char* argv[])
+{
+    CommandLineParser parser(argc, argv, params);
+    parser.about( "This program shows how to use background subtraction methods provided by "
+    " OpenCV. You can process both videos and images.\n" );
+    // The about message will be shown when printMessage is called, right before arguments table.
+    if (parser.has("help"))
+    {
+        //print help information
+        parser.printMessage();
+    }
+    //create Background Subtractor objects
+    //template<typename T> struct cv::Ptr< T > : Template class for smart pointers with shared ownership.
+    Ptr<BackgroundSubtractor> pBackSub;
+    if (parser.get<String>("algo") == "MOG2")
+        pBackSub = createBackgroundSubtractorMOG2();
+    else
+        pBackSub = createBackgroundSubtractorKNN();
+    VideoCapture capture( samples::findFile( parser.get<String>("input") ) );
+    if (!capture.isOpened()){
+        //error in opening the video input
+        cerr << "Unable to open: " << parser.get<String>("input") << endl;
+        return 0;
+    }
+    Mat frame, fgMask;
+    while (true) {
+        capture >> frame;
+    if (frame.empty())
+        break;
+    //update the background model
+    pBackSub->apply(frame, fgMask);
+    //get the frame number and write it on the current frame
+    rectangle(frame, cv::Point(10, 2), cv::Point(100,20), cv::Scalar(255,255,255), -1);
+    stringstream ss;
+    ss << capture.get(CAP_PROP_POS_FRAMES);
+    string frameNumberString = ss.str();
+    putText(frame, frameNumberString.c_str(), cv::Point(15, 15), FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
+    //show the current frame and the fg masks
+    imshow("Frame", frame);
+    imshow("FG Mask", fgMask);
+    //get the input from the keyboard
+    int keyboard = waitKey(30);
+    if (keyboard == 'q' || keyboard == 27)
+        break;
+    }
+    return 0;
+}
